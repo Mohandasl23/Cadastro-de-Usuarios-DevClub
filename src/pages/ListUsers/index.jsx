bae38e7df807c423c8e0c83d9e4142be6dbe6210
Mohandasl23 <mohandasgomes@yahoo.com.br>
2025-04-28 19:27:55 -0300

    Conclusão do projeto

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import api from "../../services/api";
 import { Button } from "../../components/Button/styles";
 import TopBackground from "../../components/TopBackground";
+import Trash from "../../assets/trash.svg";
+import { Container, ContainerUser, CardUsers, TrashIcon, Title, AvatarUser } from "./styles";
+
 
 function ListUsers(){
     const [users, setUsers] = useState([])
+    const navigate = useNavigate()
    
     useEffect(() => {
         async function getUsers(){
@@ -15,24 +20,39 @@ function ListUsers(){
         getUsers()
     }, [])
 
+    async function deleteUsers(id) {
+        await api.delete(`/usuarios/${id}`)
+        const updatedUsers = users.filter(user => user.id !== id)
+
+        setUsers(updatedUsers)
+    }
+
     // Toda vez que a tela carregar, o useEffect é executado.
     // toda vez que uma  derteminada variavel MUDA de valor, Ele é executado.
    
 
     return(
-        <div>
+        <Container>
             <TopBackground />
-            <h1>Listagem de Usuários</h1>
+            <Title>Lista de Usuários</Title>
+
+            <ContainerUser>
             {users.map((user) => (
-                <div key={user.id}>
-                    <p>Nome: {user.name}</p>
-                    <p>E-mail: {user.email}</p>
+                <CardUsers  key={user.id}>
+                    <AvatarUser src={`https://avatar.iran.liara.run/public?username=${user.id}`} alt="avatar" />
+                <div>
+                    <h3>Nome: {user.name}</h3>
                     <p>Idade: {user.age}</p>
+                    <p>E-mail: {user.email}</p>
+                    
                 </div>
+                <TrashIcon src={Trash} alt="icone-lixo" onClick={() => deleteUsers(user.id)}/> 
+                </CardUsers>
             ))}
-            <Button>Voltar</Button>
+            </ContainerUser>
+            <Button type="button" onClick={() => navigate('/')}>Voltar</Button>
             
-        </div>
+        </Container>
     )
 
 }
